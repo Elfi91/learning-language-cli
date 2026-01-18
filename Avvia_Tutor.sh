937171832df9afebc824e492767220cbbdaa5fba
Elfi91 <elisabetta.fino@gmail.com>
2026-01-18 20:41:29 +0100

    Delete Avvia_Tutor.sh

--- Avvia_Tutor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Avvio del Tutor di Italiano..."
-# Naviga nella cartella dello script
-cd "$(dirname "$0")"
-# Attiva l'ambiente virtuale
-source .venv/bin/activate
-python3 main.py
